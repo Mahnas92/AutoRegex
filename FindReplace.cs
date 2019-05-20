5c523a7ed6b88fffb647f80cae571b8088be291e
Mahmoud Nasser <git@mahmoudnasser.se>
2019-05-21 00:34:49 +0200

    Added FindReplace class
    FindReplace should hold all Find vs Replace execution pairs the programs need to
    perform.

---
+++ FindReplace.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRegex
+{
+    class FindReplace
+    {
+        private List<Pair> fnrList;
+
+        FindReplace()
+        {
+            fnrList = new List<Pair>();
+        }
+        
+        public void Add(string find, string replace)
+        {
+            fnrList.Add(new Pair(find, replace));
+        }
+    }
+
+
+    struct Pair
+    {
+        public string Find { get; }
+        public string Replace { get; }
+
+        public Pair(String find, String replace)
+        {
+            Find = find;
+            Replace = replace;
+        }
+    }
+}
