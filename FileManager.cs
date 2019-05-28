6c9f28961b591bb705473fac91a987a327c80b2d
Mahmoud Nasser <git@mahmoudnasser.se>
2019-05-28 13:53:56 +0200

    Added FileManager Class
    Added FileManager Class to handle files.

---
+++ FileManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRegex
+{
+    class FileManager
+    {
+
+        private FilePath fileSets;
+
+        public FileManager(string inputFilePath)
+        {
+            fileSets = new FilePath(inputFilePath);
+        }
+
+        public string[] ReadRegexes() { return ReadFile(fileSets.RegexFile); }
+        public string[] ReadInput() { return ReadFile(fileSets.InputFile); }
+        public void PrintOutput(String[] content) { WriteFile(fileSets.OutputFile, content); }
+
+        private string[] ReadFile(String filePath)
+        {
+            string[] lines = { "" };
+            lines = System.IO.File.ReadAllLines(filePath);
+            return lines;
+        }
+
+        private void WriteFile(String filePath, String[] output)
+        {
+            System.IO.File.WriteAllLines(filePath, output);
+        }
+
+        struct FilePath
+        {
+            public String InputFile { get; set; }
+            public String RegexFile { get; set; }
+            public String OutputFile { get; set; }
+
+            public FilePath(String inputFilePath)
+            {
+                InputFile = inputFilePath;
+                RegexFile = inputFilePath.Replace(".txt", "") + ".regex";
+                OutputFile = inputFilePath.Replace(".txt", "") + ".output.txt";
+            }
+
+        }
+    }
+}
